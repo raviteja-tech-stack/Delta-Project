1c4f0729ab68263c577ca09de9ce36943b7f333d
Raviteja <padigelaraviteja47@gmail.com>
2025-08-19 15:40:07 +0530

    made some changes to map

--- public/JS/map.js
+++ public/JS/map.js
@@ -9,7 +9,7 @@ const marker1 = new mapboxgl.Marker({ color: "red" })
   .setLngLat(listing.geometry.coordinates)
   .setPopup(
     new mapboxgl.Popup({ offset: 25 }).setHTML(
-      `<h4>${listing.title}<h4><p>Exact location will be provided after booking<p>`
+      `<h4>${listing.title}</h4><p>Exact location will be provided after booking<p>`
     )
   )
   .addTo(map);
